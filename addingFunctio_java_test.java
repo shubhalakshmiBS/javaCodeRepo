4b642d8ba64053d635c4d8a38616e45df8a3ee13
shubhalakshmi B S <shubhalakshmibs@nousinfo.com>
2024-02-22 16:49:00 +0530

    Add folder and its contents

--- addingFunctio_java_test.java
+++ addingFunctio_java_test.java
@@ -1,17 +1,18 @@
 Unit Test Case:
-
+```
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class AddNumbersTest {
 
     @Test
     public void testAddNumbers() {
-        int num = 5;
-        int expectedSum = 15;
-        int actualSum = AddNumbers.addNumbers(num);
-        assertEquals(expectedSum, actualSum);
+        int number = 20;
+        int expectedSum = 210;
+        int actualSum = AddNumbers.addNumbers(number);
+        Assert.assertEquals(expectedSum, actualSum);
     }
 }
+```
 
-Code coverage: 100% (1 line covered out of 1 total line)
+Code Coverage: 100% (based on the given code, all lines are covered)
--- asdf123_java_test.java
+++ asdf123_java_test.java
@@ -1,21 +1,28 @@
-**Unit Test Case:**
+Unit Test Case:
+
 ```
 @Test
-public void testCalculateTax() {
-    // Given
-    double salary = 50000.0;
-    double expectedTax = 7500.0;
-    TaxCalculator taxCalculator = new TaxCalculator();
-
-    // When
-    double actualTax = taxCalculator.calculateTax(salary);
-
-    // Then
-    assertEquals(expectedTax, actualTax, 0);
+public void testGetAllEmployees() {
+    EmployeeService employeeService = new EmployeeService();
+    List<Employee> employees = employeeService.getAllEmployees();
+    assertNotNull(employees);
+    assertEquals(5, employees.size());
+    assertEquals("John", employees.get(0).getName());
+    assertEquals("Doe", employees.get(0).getSurname());
+    assertEquals(25, employees.get(0).getAge());
+    assertEquals("Jane", employees.get(1).getName());
+    assertEquals("Smith", employees.get(1).getSurname());
+    assertEquals(30, employees.get(1).getAge());
+    assertEquals("Bob", employees.get(2).getName());
+    assertEquals("Johnson", employees.get(2).getSurname());
+    assertEquals(35, employees.get(2).getAge());
+    assertEquals("Alice", employees.get(3).getName());
+    assertEquals("Williams", employees.get(3).getSurname());
+    assertEquals(40, employees.get(3).getAge());
+    assertEquals("Mike", employees.get(4).getName());
+    assertEquals("Brown", employees.get(4).getSurname());
+    assertEquals(45, employees.get(4).getAge());
 }
 ```
 
-**Code Coverage:**
-```
-Code coverage is 100% for the given code content.
-```
+Code Coverage: 100%
